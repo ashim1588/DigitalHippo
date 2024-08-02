d2d454ab474aa2a2df8a1269f0778abdb94ff8db
ashim1588 <ashimmehar158@gmail.com>
2024-08-02 14:54:49 +0530

    loggers typo server.ts

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -11,12 +11,16 @@ import {useForm} from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import { AuthCredentialsValidator, TAuthCredentialsValidator } from '@/lib/validators/account-credentials-validator';
+import { trpc } from '@/trpc/client';
 
 const Page = () => {
 
     const { register, handleSubmit, formState: {errors} } = useForm<TAuthCredentialsValidator>({
         resolver: zodResolver(AuthCredentialsValidator),
     });
+
+    const { data } = trpc.anyApiRoute.useQuery()
+    console.log(data)
 
     const onSubmit = ({email, password}:  TAuthCredentialsValidator) => {
 
