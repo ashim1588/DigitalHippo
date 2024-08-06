7e3a35bc52fc4bb90740391b012289c505274af4
ashim1588 <ashimmehar158@gmail.com>
2024-08-06 12:17:24 +0530

    Orders Collection product file collection

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,6 +5,9 @@ import { slateEditor } from "@payloadcms/richtext-slate"
 import path from "path";
 import { Users } from "./collections/Users";
 import dotenv from "dotenv";
+import { Products } from "./collections/Products/Products";
+import { Media } from "./collections/Media";
+import { ProductFiles } from "./collections/ProductFile";
 
 dotenv.config({
     path: path.resolve(__dirname, "../.env")
@@ -12,7 +15,7 @@ dotenv.config({
 
 export default buildConfig ({
     serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-    collections: [Users],
+    collections: [Users, Products, Media, ProductFiles],
     routes: {
         admin: '/sell'
     },
@@ -33,6 +36,6 @@ export default buildConfig ({
         url: process.env.MONGODB_URL!,
     }),
     typescript: {
-        outputFile: path.resolve(__dirname, "src", "payload.types.ts"),
+        outputFile: path.resolve(__dirname, 'payload-types.ts'),
     }
 });
